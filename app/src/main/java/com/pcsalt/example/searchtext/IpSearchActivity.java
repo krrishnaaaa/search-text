61233dc8f40c2a2acb38e862fb89828c75b979e1
krrishnaaaa <krishna@pcsalt.com>
2016-11-02 20:20:17 +0530

    created activity for state and ip search and navigating to screen

---
+++ app/src/main/java/com/pcsalt/example/searchtext/IpSearchActivity.java
@@ -0,0 +1,18 @@
+package com.pcsalt.example.searchtext;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Navkrishna on 02 November, 2016
+ */
+
+public class IpSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ip_search);
+    }
+}
