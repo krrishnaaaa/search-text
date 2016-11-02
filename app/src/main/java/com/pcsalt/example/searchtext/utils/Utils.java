fbd40b78ffadf9225dc01ea2bd5545b55b246483
krrishnaaaa <krishna@pcsalt.com>
2016-11-02 22:41:43 +0530

    added utility method to display relevant error message after api call failure

--- app/src/main/java/com/pcsalt/example/searchtext/utils/Utils.java
+++ app/src/main/java/com/pcsalt/example/searchtext/utils/Utils.java
@@ -1,6 +1,14 @@
 package com.pcsalt.example.searchtext.utils;
 
+import android.content.Context;
 import android.widget.TextView;
+
+import com.pcsalt.example.searchtext.BuildConfig;
+import com.pcsalt.example.searchtext.R;
+
+import java.io.IOException;
+
+import retrofit2.adapter.rxjava.HttpException;
 
 /**
  * Created by Navkrishna on 02 November, 2016
@@ -14,5 +22,18 @@ public final class Utils {
         if (textView == null)
             return "";
         return textView.getText().toString().trim();
+    }
+
+    public static String errorHandleResponse(Throwable throwable, Context context) {
+        if (BuildConfig.DEBUG) {
+            throwable.printStackTrace();
+        }
+        if (throwable instanceof HttpException) {
+            return context.getString(R.string.err_connection_lost);
+        } else if (throwable instanceof IOException) {
+            return context.getString(R.string.err_no_network);
+        } else {
+            return context.getString(R.string.err_connection_lost);
+        }
     }
 }
